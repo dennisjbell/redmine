ebe087106c64cce5540e70ad54b60dd1825e2ba0
Dennis Bell <dennis.j.bell@gmail.com>
2017-09-12 11:20:48 -0700

    Refinements to habitatification

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -6,3 +6,8 @@ pkg_version="0.1.0"
 pkg_binds_optional=( [database]="port" )
 
 pkg_deps=(core/imagemagick)
+
+do_install() {
+  export DATABASE_URL="postgresql://nobody@nowhere/fake_db_to_appease_rails_env"
+  do_default_install
+}
